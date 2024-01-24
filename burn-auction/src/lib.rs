56172648ebef43e1922b4377a7340058c49008a6
Robert Zaremba <robert@zaremba.ch>
2024-01-24 14:17:59 +0100

    burn-auction bootstrap

--- burn-auction/src/lib.rs
+++ burn-auction/src/lib.rs
@@ -1,18 +1,6 @@
-use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
-};
+pub mod contract;
+mod error;
+pub mod msg;
+pub mod state;
 
-#[entry_point]
-pub fn instantiate(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: Empty,
-) -> StdResult<Response> {
-    Ok(Response::new())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-}
+pub use crate::error::ContractError;
